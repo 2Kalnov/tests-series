f6ef080cc7cb2d92157860424ffe777f85682c7c
Nikita Kalnov <jdmcorolla21@gmail.com>
2019-04-01 22:07:23 +0400

    Updates

---
+++ precache-manifest.0c8c6cbb8d2d9105b3830a6d85dbd0e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17687832d78502085d9c",
+    "url": "/tests-series/static/js/runtime~main.499518cd.js"
+  },
+  {
+    "revision": "1efcc0ef99b076f0b69d",
+    "url": "/tests-series/static/js/main.5c06ab12.chunk.js"
+  },
+  {
+    "revision": "b5ca37dc55ecc90c968c",
+    "url": "/tests-series/static/js/2.f7c95f5c.chunk.js"
+  },
+  {
+    "revision": "1efcc0ef99b076f0b69d",
+    "url": "/tests-series/static/css/main.8394178d.chunk.css"
+  },
+  {
+    "revision": "0261fe66fad3714863cbd1e60e0ba14e",
+    "url": "/tests-series/index.html"
+  }
+];
